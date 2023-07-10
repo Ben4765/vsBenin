a560b4d580e3d494af0c94ba5b216d6a573d136c
Ben4765 <138756649+Ben4765@users.noreply.github.com>
2023-07-10 09:47:57 +0530

    Update Aithmetic.java

--- Aithmetic.java
+++ Aithmetic.java
@@ -2,7 +2,7 @@ package assignment;
 
 import java.util.Scanner;
 
-public class Arithmetic {
+public class Aithmetic {
 	
 	int add(int a,int b) {
 		return(a+b);
@@ -13,22 +13,29 @@ public class Arithmetic {
 	int mul(int a,int b) {
 		return(a*b);
 	}
-	int div(int a,int b) {
-		try {
+	double div(int a,int b) {
 		return(a/b);
-		}
-		catch(){
-			
-		}
 	}
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		Arithmetic obj=new Arithmetic();
+		Aithmetic obj=new Aithmetic();
 		System.out.println("Enter two numbers to find the sum");
 		int n=sc.nextInt();
 		int m=sc.nextInt();
-		System.out.println("The sum is "+ obj.add(n, m);
+		System.out.println("The sum is "+obj.add(m, n));
+		System.out.println("Enter two numbers to find the difference");
+		n=sc.nextInt();
+		m=sc.nextInt();
+		System.out.println("The difference is "+obj.sub(m, n));
+		System.out.println("Enter two numbers to find the product");
+		n=sc.nextInt();
+		m=sc.nextInt();
+		System.out.println("The product is "+obj.mul(m, n));
+		System.out.println("Enter two numbers to find the quotient");
+		n=sc.nextInt();
+		m=sc.nextInt();
+		System.out.println("The quotient is "+obj.div(m, n));
 	}
 
 }
